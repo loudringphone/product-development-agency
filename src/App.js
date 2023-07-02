b609e6749cc03429919315578a2c171077dcebdf
loudringphone <loudringphone@gmail.com>
2023-07-02 13:28:15 +1000

    scroll to top

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React, { useEffect } from "react";
+import { useLocation } from 'react-router-dom';
 import Header from './components/header/Header';
-import Routers from './routers/Routers'
+import Routers from './routers/Routers';
 import Footer from './components/UI/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ScrollToTop />
       <main>
           <Routers />
       </main>
@@ -15,3 +18,15 @@ function App() {
 }
 
 export default App;
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+      document.documentElement.scrollTo({
+      // In general, if you want to scroll to the top of the page, using document.documentElement.scrollTop is the more reliable and widely used approach. However, if you need to scroll to a specific position on the page or use more advanced scrolling behaviors, window.scrollTo or window.scrollBy may be more appropriate.
+      top: 0,
+      left: 0,
+      behavior: "instant",
+      });
+  }, [pathname])
+} 
