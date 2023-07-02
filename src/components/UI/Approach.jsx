2f1cf1a0e9c2702c152b7e73e857eac686febe97
darcyhmansfield <darcyhmansfield@gmail.com>
2023-07-02 10:08:15 +1000

    Added images to home page and updated Purpose and Approach components

--- src/components/UI/Approach.jsx
+++ src/components/UI/Approach.jsx
@@ -13,6 +13,28 @@ const Approach = () => {
                     <p>... and even after that, if you like! We know creating something that will be the first (and maybe only) touchpoint between you and your audience is a big deal! That’s why we have a talented cross-functional team to help you with what you need.</p>
                 </div>
             </div>
+            <div className='approach-cards'>
+                <div className='approach-card'>
+                    <img src="/Untitled_Artwork 140 1.png" alt="" />
+                    <h3>We’ll set the stage...</h3>
+                    <p>And create workshops where you are empowered to co-create and design with us! We’re also here for you after, and check-in regularly to ensure your continued success with our delivered solutions.</p>
+                </div>
+                <div className='approach-card'>
+                    <img src="/Untitled_Artwork 138 1.png" alt="" />
+                    <h3>We’ll find out what you need to know.</h3>
+                    <p>We’ll conduct thorough research to  provide you with evidence-based insights and solutions that can help you engage, inspire and connect with your audience.</p>
+                </div>
+                <div className='approach-card'>
+                    <img src="/Untitled_Artwork 141 1.png" alt="" />
+                    <h3>We’ll build a custom product for you.</h3>
+                    <p>Our team of skilled developers will take care of everything from coding to UI design, ensuring a seamless and responsive browsing experience for your visitors.</p>
+                </div>
+                <div className='approach-card'>
+                    <img src="/Untitled_Artwork 139 1.png" alt="" />
+                    <h3>We’ll make it look great!</h3>
+                    <p>Our in-house visual designer/illustrator will thoughtfully create a memorable and appealing visual language that captures the essence of your brand, and leave a lasting impression on your audience. </p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/UI/PurposeCard.jsx
+++ src/components/UI/PurposeCard.jsx
@@ -6,19 +6,23 @@ const Purpose = () => {
         <div className="purpose-container">
             <div className='purpose-cards'>
                 <div className="purpose-card">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Earth_Western_Hemisphere_transparent_background.png/1200px-Earth_Western_Hemisphere_transparent_background.png" alt="" />
+                    <img src="/Untitled_Artwork 142 1.png" alt="" />
                     <p>We design for people and the planet</p>
                 </div>
                 <div className="purpose-card">
-                    <img src="https://freepngimg.com/download/book/21947-1-open-book-transparent-background.png" alt="" />
+                    <img src="/Untitled_Artwork 144 1.png" alt="" />
                     <p>We want to help you tell your story</p>
                 </div>
                 <div className="purpose-card">
-                    <img src="" alt="" />
+                    <img src="/Untitled_Artwork 145 1.png" alt="" />
                     <p>We strive for excellence in everything we do</p>
                 </div>
             </div>
-            <div>
+            <div className='ideas-cards'>
+                <div className='ideas-card'>
+
+
+                </div>
 
             </div>
         </div>
