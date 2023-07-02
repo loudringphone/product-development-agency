6efc06a29a68fe72d17ec3da77f135d764b4554c
darcyhmansfield <darcyhmansfield@gmail.com>
2023-07-02 11:25:32 +1000

    updated css for home page

--- src/components/UI/Approach.jsx
+++ src/components/UI/Approach.jsx
@@ -15,22 +15,22 @@ const Approach = () => {
             </div>
             <div className='approach-cards'>
                 <div className='approach-card'>
-                    <img src="/Untitled_Artwork 140 1.png" alt="" />
+                    <div className='approach-image'><img src="/Untitled_Artwork 140 1.png" alt="" /></div>
                     <h3>We’ll set the stage...</h3>
                     <p>And create workshops where you are empowered to co-create and design with us! We’re also here for you after, and check-in regularly to ensure your continued success with our delivered solutions.</p>
                 </div>
                 <div className='approach-card'>
-                    <img src="/Untitled_Artwork 138 1.png" alt="" />
+                    <div className='approach-image'><img src="/Untitled_Artwork 138 1.png" alt="" /></div>
                     <h3>We’ll find out what you need to know.</h3>
                     <p>We’ll conduct thorough research to  provide you with evidence-based insights and solutions that can help you engage, inspire and connect with your audience.</p>
                 </div>
                 <div className='approach-card'>
-                    <img src="/Untitled_Artwork 141 1.png" alt="" />
+                    <div className='approach-image'><img src="/Untitled_Artwork 141 1.png" alt="" /></div>
                     <h3>We’ll build a custom product for you.</h3>
                     <p>Our team of skilled developers will take care of everything from coding to UI design, ensuring a seamless and responsive browsing experience for your visitors.</p>
                 </div>
                 <div className='approach-card'>
-                    <img src="/Untitled_Artwork 139 1.png" alt="" />
+                    <div className='approach-image'><img src="/Untitled_Artwork 139 1.png" alt="" /></div>
                     <h3>We’ll make it look great!</h3>
                     <p>Our in-house visual designer/illustrator will thoughtfully create a memorable and appealing visual language that captures the essence of your brand, and leave a lasting impression on your audience. </p>
                 </div>
